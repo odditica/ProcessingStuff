9554805613923fefee162c9510a96efd9da083fa
Jan Vorisek <vorisek.jan@outlook.com>
2018-04-07 11:30:32 +0200

    Shader cleanup.

--- ForkIt/frag.glsl
+++ ForkIt/frag.glsl
@@ -1,6 +1,5 @@
 varying flat vec3 vertNormal;
 varying flat vec4 vertColor;
-varying vec4 vertTexCoord;
 
 void main() {
   gl_FragColor = vec4(0., 0., 0., .9);
--- ForkIt/vertex.glsl
+++ ForkIt/vertex.glsl
@@ -11,22 +11,12 @@ attribute vec3 normal;
 
 flat varying vec3 vertNormal;
 flat varying vec4 vertColor;
-flat varying vec4 vertTexCoord;
-flat varying vec4 screenCoord;
-
+varying vec4 vertTexCoord;
+varying vec4 screenCoord;
 
 void main() {
   vertColor = color;
   vertTexCoord = texMatrix * vec4(texCoord, 1.0, 1.0);
   vertNormal = normalize(normalMatrix * normal);
   gl_Position = transform * (position + vec4(normalize(normal) * .15, 0.));
-
-  
-  //gl_Position.xyz += vertNormal * 20.5;
-  //screenCoord = position * transform;
-
-  //screenCoord.xy /= 1. / position.w;
-  
- 
-
 }
